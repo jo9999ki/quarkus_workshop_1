f213b8820f93a67101d45f6c6aa5a3ee8c038a70
jkirchner <jkirchner@HW1264.s-und-n.de>
2020-06-08 14:26:56 +0200

    Initial commit

---
+++ src/test/java/de/sn/quarkus/businessfunctions/NativeBFResourceIT.java
@@ -0,0 +1,9 @@
+package de.sn.quarkus.businessfunctions;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeBFResourceIT extends BFResourceTest {
+
+    // Execute the same tests but in native mode.
+}
